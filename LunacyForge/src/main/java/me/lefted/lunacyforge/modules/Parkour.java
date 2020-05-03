eab992421f9ab43eaf645cc9f762e4e0e9aaa375
Lefted <GartnerMoritz@yahoo.de>
2020-05-03 11:24:05 +0200

    fixed bug that lets you jump under water

--- LunacyForge/src/main/java/me/lefted/lunacyforge/modules/Parkour.java
+++ LunacyForge/src/main/java/me/lefted/lunacyforge/modules/Parkour.java
@@ -17,7 +17,7 @@ public class Parkour extends Module {
 
     @EventTarget
     public void onUpdate(UpdateEvent event) {
-	if (mc.thePlayer.onGround && !mc.thePlayer.isSneaking() && mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox()
+	if (!mc.thePlayer.isInWater() && !mc.thePlayer.isInLava() && mc.thePlayer.onGround && !mc.thePlayer.isSneaking() && mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox()
 	    .offset(0.0D, -0.5D, 0.0D).contract(0.001D, 0.0D, 0.001D)).isEmpty() && ((Minecraft.getMinecraft()).thePlayer.isCollidedVertically || !(Minecraft
 		.getMinecraft()).thePlayer.isAirBorne)) {
 	    mc.thePlayer.jump();
