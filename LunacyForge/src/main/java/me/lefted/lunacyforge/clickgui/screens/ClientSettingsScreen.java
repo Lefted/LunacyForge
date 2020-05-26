03452a394e0f6212adae82f57becb9173baf497e
Lefted <GartnerMoritz@yahoo.de>
2020-05-26 18:30:44 +0200

    Aesthetic changes to Client Settings

--- LunacyForge/src/main/java/me/lefted/lunacyforge/clickgui/screens/ClientSettingsScreen.java
+++ LunacyForge/src/main/java/me/lefted/lunacyforge/clickgui/screens/ClientSettingsScreen.java
@@ -48,21 +48,42 @@ public class ClientSettingsScreen extends SettingsScreen {
 	// use client
 	SettingContainer enabledContainer = new SettingContainer();
 	enabledContainer.centerX();
-	enabledContainer.setDescription("Use client");
-
+	enabledContainer.setDescription("Use Client");
 	ContainerCheckbox enabledCheckbox = new ContainerCheckbox(ClientConfig.isEnabled());
 	enabledCheckbox.setPosX(enabledContainer.getPosX() + enabledContainer.getWidth() - enabledCheckbox.WIDTH - 10);
 	enabledCheckbox.setConsumer(newValue -> {
 	    ClientConfig.setEnabled(newValue);
 	    ClientConfig.saveConfig();
 	});
-
 	enabledContainer.setSettingOffsetY(7);
 	enabledContainer.setSettingElement(enabledCheckbox);
 	settings.add(enabledContainer);
 
 	// adds the settings that determine the client color
-	addRGBContainer(settings);
+	// addRGBContainer(settings);
+
+	// clickgui keybind
+	SettingContainer keybindContainer = new SettingContainer();
+	keybindContainer.centerX();
+	keybindContainer.setDescription("Clickgui Keybind");
+	keybindContainer.setSettingOffsetY(7);
+	ContainerKeybind keybind = new ContainerKeybind(this, 130, 15, ModuleManager.getModule(ClickGui.class).getKeycode());
+	keybind.setPosX(keybindContainer.getPosX() + keybindContainer.getWidth() - keybind.getWidth() - 8);
+	keybind.setIntConsumer(newValue -> ModuleManager.getModule(ClickGui.class).setKeycode(newValue));
+	keybindContainer.setSettingElement(keybind);
+	settings.add(keybindContainer);
+
+	// client color
+	SettingContainer clientColorContainer = new SettingContainer();
+	ContainerColorpicker clientColorPicker = new ContainerColorpicker(this, clientColorContainer, ClientConfig.getGuiColor());
+	clientColorContainer.centerX();
+	clientColorPicker.setPosX(clientColorContainer.getPosX() + clientColorContainer.getWidth() - clientColorPicker.getWidth() - 71);
+	clientColorPicker.setColorObjConsumer(color -> ClientConfig.setGuiColor(color));
+	clientColorPicker.setHasAlpha(false);
+	clientColorContainer.setDescription("Color Accent");
+	clientColorContainer.setSettingOffsetY(10);
+	clientColorContainer.setSettingElement(clientColorPicker);
+	settings.add(clientColorContainer);
 
 	// mode
 	SettingContainer modeContainer = new SettingContainer();
@@ -79,27 +100,10 @@ public class ClientSettingsScreen extends SettingsScreen {
 	modeContainer.setBackgroundLevel(1);
 	settings.add(modeContainer);
 
-	// // arraylist
-	// SettingContainer arraylistContainer = new SettingContainer();
-	// arraylistContainer.centerX();
-	// arraylistContainer.setDescription("Render Arraylist");
-	//
-	// ContainerCheckbox arraylistCheckbox = new ContainerCheckbox(ClientConfig.isRenderArrayList());
-	// arraylistCheckbox.setPosX(arraylistContainer.getPosX() + arraylistContainer.getWidth() - arraylistCheckbox.WIDTH - 10);
-	// arraylistCheckbox.setConsumer(newValue -> {
-	// ClientConfig.setRenderArrayList(newValue);
-	// ClientConfig.saveConfig();
-	// });
-	//
-	// arraylistContainer.setSettingOffsetY(7);
-	// arraylistContainer.setSettingElement(arraylistCheckbox);
-	// settings.add(arraylistContainer);
-
 	// arraylist mode
 	SettingContainer arraylistBgContainer = new SettingContainer();
 	arraylistBgContainer.centerX();
 	arraylistBgContainer.setDescription("Arraylist Background");
-
 	ContainerComobox arraylistBgCombobox = new ContainerComobox(arraylistBgContainer, ClientConfig.getArraylistMode(), "None", "Normal", "Shadow",
 	    "Normal+Rect", "Shadow+Rect");
 	arraylistBgCombobox.setPosX(arraylistBgContainer.getPosX() + arraylistBgContainer.getWidth() - arraylistBgCombobox.ENTRY_WIDTH - 10);
@@ -107,7 +111,6 @@ public class ClientSettingsScreen extends SettingsScreen {
 	    ClientConfig.setArraylistMode(newValue);
 	    ClientConfig.saveConfig();
 	});
-
 	arraylistBgContainer.setSettingOffsetY(8);
 	arraylistBgContainer.setBackgroundLevel(1);
 	arraylistBgContainer.setSettingElement(arraylistBgCombobox);
@@ -116,44 +119,16 @@ public class ClientSettingsScreen extends SettingsScreen {
 	// announce module toggle
 	SettingContainer chatContainer = new SettingContainer();
 	chatContainer.centerX();
-	chatContainer.setDescription("Print in chat if enabling/disabling a module");
-
+	chatContainer.setDescription("Notify In Chat When Enabling/Disabling A Module");
 	ContainerCheckbox chatCheckbox = new ContainerCheckbox(ClientConfig.isAnnounceModuleToggle());
 	chatCheckbox.setPosX(chatContainer.getPosX() + chatContainer.getWidth() - chatCheckbox.WIDTH - 10);
 	chatCheckbox.setConsumer(newValue -> {
 	    ClientConfig.setAnnounceModuleToggle(newValue);
 	    ClientConfig.saveConfig();
 	});
-
 	chatContainer.setSettingOffsetY(7);
 	chatContainer.setSettingElement(chatCheckbox);
 	settings.add(chatContainer);
-
-	// clickgui keybind
-	SettingContainer keybindContainer = new SettingContainer();
-	keybindContainer.centerX();
-	keybindContainer.setDescription("Clickgui Keybind");
-	keybindContainer.setSettingOffsetY(7);
-
-	ContainerKeybind keybind = new ContainerKeybind(this, 130, 15, ModuleManager.getModule(ClickGui.class).getKeycode());
-	keybind.setPosX(keybindContainer.getPosX() + keybindContainer.getWidth() - keybind.getWidth() - 8);
-
-	keybind.setIntConsumer(newValue -> ModuleManager.getModule(ClickGui.class).setKeycode(newValue));
-
-	keybindContainer.setSettingElement(keybind);
-	settings.add(keybindContainer);
-
-	// DEBUG
-	SettingContainer pickerContainer = new SettingContainer();
-	pickerContainer.centerX();
-	pickerContainer.setDescription("Color Test");
-
-	ContainerColorpicker picker = new ContainerColorpicker(this, pickerContainer, Color.CYAN);
-	picker.setPosX(pickerContainer.getPosX() + pickerContainer.getWidth() - picker.getWidth() - 71);
-
-	pickerContainer.setSettingOffsetY(10);
-	pickerContainer.setSettingElement(picker);
-	settings.add(pickerContainer);
     }
 
     @Override
@@ -207,65 +182,65 @@ public class ClientSettingsScreen extends SettingsScreen {
     private void addRGBContainer(ArrayList<SettingContainer> settings) {
 	// final ScaledResolution sc = new ScaledResolution(Minecraft.getMinecraft());
 
-	// description
-	SettingContainer title = new SettingContainer(350, 20);
-	title.centerX();
-	// title.setPosX(sc.getScaledWidth() / 2 - title.getWidth() / 2);
-	title.setDescription("Colors");
-	settings.add(title);
-
-	// red
-	ContainerSlider sliderRed = new ContainerSlider(this, ContainerSlider.NumberType.INTEGER, 0, 255, 1D);
-	sliderRed.setValue(ClientConfig.getGuiColor().getRed());
-	sliderRed.setConsumer((d) -> {
-	    colorRed.setDescription("Red: " + sliderRed.getValueString());
-	    updateGuiColor();
-	});
-
-	colorRed = new SettingContainer(350, 20);
-	colorRed.setSettingOffsetY(10);
-	colorRed.setDescription("Red: " + ClientConfig.getGuiColor().getRed());
-
-	colorRed.centerX();
-	sliderRed.setPosX(colorRed.getPosX() + colorRed.getWidth() - sliderRed.WIDTH - 10);
-	colorRed.setSettingElement(sliderRed);
-	settings.add(colorRed);
-
-	// green
-	ContainerSlider sliderGreen = new ContainerSlider(this, ContainerSlider.NumberType.INTEGER, 0, 255, 1D);
-	sliderGreen.setValue(ClientConfig.getGuiColor().getGreen());
-	sliderGreen.setConsumer((d) -> {
-	    colorGreen.setDescription("Green: " + sliderGreen.getValueString());
-	    updateGuiColor();
-	});
-
-	colorGreen = new SettingContainer(350, 20);
-	colorGreen.setSettingOffsetY(10);
-	colorGreen.setDescription("Green: " + ClientConfig.getGuiColor().getGreen());
-
-	colorGreen.centerX();
-	sliderGreen.setPosX(colorGreen.getPosX() + colorGreen.getWidth() - sliderGreen.WIDTH - 10);
-	colorGreen.setSettingElement(sliderGreen);
-	settings.add(colorGreen);
-
-	// blue
-	ContainerSlider sliderBlue = new ContainerSlider(this, ContainerSlider.NumberType.INTEGER, 0, 255, 1D);
-	sliderBlue.setValue(ClientConfig.getGuiColor().getBlue());
-	sliderBlue.setConsumer((d) -> {
-	    colorBlue.setDescription("Blue: " + sliderBlue.getValueString());
-	    updateGuiColor();
-	});
-
-	colorBlue = new SettingContainer();
-	colorBlue.setSettingOffsetY(10);
-	colorBlue.setDescription("Blue: " + ClientConfig.getGuiColor().getBlue());
-
-	colorBlue.centerX();
-	sliderBlue.setPosX(colorBlue.getPosX() + colorBlue.getWidth() - sliderBlue.WIDTH - 10);
-	colorBlue.setSettingElement(sliderBlue);
-	settings.add(colorBlue);
-
-	groupSettings(title, colorRed, colorGreen, colorBlue);
+	// // description
+	// SettingContainer title = new SettingContainer(350, 20);
+	// title.centerX();
+	// // title.setPosX(sc.getScaledWidth() / 2 - title.getWidth() / 2);
+	// title.setDescription("Colors");
+	// settings.add(title);
+	//
+	// // red
+	// ContainerSlider sliderRed = new ContainerSlider(this, ContainerSlider.NumberType.INTEGER, 0, 255, 1D);
+	// sliderRed.setValue(ClientConfig.getGuiColor().getRed());
+	// sliderRed.setConsumer((d) -> {
+	// colorRed.setDescription("Red: " + sliderRed.getValueString());
+	// updateGuiColor();
+	// });
+	//
+	// colorRed = new SettingContainer(350, 20);
+	// colorRed.setSettingOffsetY(10);
+	// colorRed.setDescription("Red: " + ClientConfig.getGuiColor().getRed());
+	//
+	// colorRed.centerX();
+	// sliderRed.setPosX(colorRed.getPosX() + colorRed.getWidth() - sliderRed.WIDTH - 10);
+	// colorRed.setSettingElement(sliderRed);
+	// settings.add(colorRed);
+	//
+	// // green
+	// ContainerSlider sliderGreen = new ContainerSlider(this, ContainerSlider.NumberType.INTEGER, 0, 255, 1D);
+	// sliderGreen.setValue(ClientConfig.getGuiColor().getGreen());
+	// sliderGreen.setConsumer((d) -> {
+	// colorGreen.setDescription("Green: " + sliderGreen.getValueString());
+	// updateGuiColor();
+	// });
+	//
+	// colorGreen = new SettingContainer(350, 20);
+	// colorGreen.setSettingOffsetY(10);
+	// colorGreen.setDescription("Green: " + ClientConfig.getGuiColor().getGreen());
+	//
+	// colorGreen.centerX();
+	// sliderGreen.setPosX(colorGreen.getPosX() + colorGreen.getWidth() - sliderGreen.WIDTH - 10);
+	// colorGreen.setSettingElement(sliderGreen);
+	// settings.add(colorGreen);
+	//
+	// // blue
+	// ContainerSlider sliderBlue = new ContainerSlider(this, ContainerSlider.NumberType.INTEGER, 0, 255, 1D);
+	// sliderBlue.setValue(ClientConfig.getGuiColor().getBlue());
+	// sliderBlue.setConsumer((d) -> {
+	// colorBlue.setDescription("Blue: " + sliderBlue.getValueString());
+	// updateGuiColor();
+	// });
+	//
+	// colorBlue = new SettingContainer();
+	// colorBlue.setSettingOffsetY(10);
+	// colorBlue.setDescription("Blue: " + ClientConfig.getGuiColor().getBlue());
+	//
+	// colorBlue.centerX();
+	// sliderBlue.setPosX(colorBlue.getPosX() + colorBlue.getWidth() - sliderBlue.WIDTH - 10);
+	// colorBlue.setSettingElement(sliderBlue);
+	// settings.add(colorBlue);
+	//
+	// groupSettings(title, colorRed, colorGreen, colorBlue);
     }
 
     private void updateGuiColor() {
